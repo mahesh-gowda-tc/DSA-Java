281670e2562969231045b431e3213516c6e20d49
Mahesh Gowda T C <adm23cse7248@sjbit.edu.in>
2026-07-17 18:30:32 +0530

    Add SQL query to combine person and address tables

---
+++ SQL/LeetCode-175-Combine-Two-Tables.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+person.firstName,
+person.lastName,
+Address.city,
+Address.state
+FROM person
+LEFT JOIN Address
+ON person.personId=Address.personId;
